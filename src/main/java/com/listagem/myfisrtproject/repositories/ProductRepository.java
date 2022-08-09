6908aa8759e2353a733698fee8f24aaa393343c4
matheuskamers <mkamersandrade@gmail.com>
2022-08-09 14:11:48 -0300

    Insert product, in memory

---
+++ src/main/java/com/listagem/myfisrtproject/repositories/ProductRepository.java
@@ -0,0 +1,28 @@
+package com.listagem.myfisrtproject.repositories;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.listagem.myfisrtproject.entities.Product;
+
+@Component
+public class ProductRepository {
+	
+	private Map<Long, Product> map = new HashMap<>();
+	
+	public void save(Product obj) {
+		map.put(obj.getId(), obj);
+	}
+
+	public Product findById(Long id) {
+		return map.get(id);
+	}
+	
+	public List<Product> findAll() {
+		return new ArrayList<Product>(map.values());
+	}
+}
