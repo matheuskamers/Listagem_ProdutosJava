c3cd116fd34043c65e89ee15535f863165858d47
matheuskamers <mkamersandrade@gmail.com>
2022-08-09 13:47:15 -0300

    First commit, implements Category

---
+++ src/main/java/com/listagem/myfisrtproject/MyfisrtprojectApplication.java
@@ -0,0 +1,30 @@
+package com.listagem.myfisrtproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.listagem.myfisrtproject.entities.Category;
+import com.listagem.myfisrtproject.repositories.CategoryRepository;
+
+@SpringBootApplication
+public class MyfisrtprojectApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MyfisrtprojectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
+		
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
+	}
+
+}
---
+++ src/test/java/com/listagem/myfisrtproject/MyfisrtprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listagem.myfisrtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfisrtprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
